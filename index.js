82f7b3273cfd215fd318d449b83b14884214cbea
dexterlandry15 <dexter.landry15@gmail.com>
2022-10-16 07:58:08 +0300

    server

--- index.js
+++ index.js
@@ -1,76 +1,10 @@
 require('dotenv').config()
 const SpotifyWebApi = require('spotify-web-api-node');
 const TwitchBot = require('twitch-bot')
+const start_server = require('./server')
+const { spotifyApi } = require('./spotify')
 
-const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENTID,
-  clientSecret: process.env.SPOTIFY_CLIENTSECRET,
-  redirectUri: 'http://localhost/callback'
-});
-
-
-const authorizationCode = process.env.SPOTIFY_CODE;
-
-// When our access token will expire
-let tokenExpirationEpoch;
-
-// First retrieve an access token
-spotifyApi.authorizationCodeGrant(authorizationCode).then(
-  function(data) {
-    // Set the access token and refresh token
-    spotifyApi.setAccessToken(data.body['access_token']);
-    spotifyApi.setRefreshToken(data.body['refresh_token']);
-
-    // Save the amount of seconds until the access token expired
-    tokenExpirationEpoch =
-      new Date().getTime() / 1000 + data.body['expires_in'];
-    console.log(
-      'Retrieved token. It expires in ' +
-        Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
-        ' seconds!'
-    );
-  },
-  function(err) {
-    console.log(
-      'Something went wrong when retrieving the access token!',
-      err.message
-    );
-  }
-);
-
-// Continually print out the time left until the token expires..
-let numberOfTimesUpdated = 0;
-
-setInterval(function() {
-  console.log(
-    'Time left: ' +
-      Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
-      ' seconds left!'
-  );
-
-  // OK, we need to refresh the token. Stop printing and refresh.
-  if (++numberOfTimesUpdated > 5) {
-    clearInterval(this);
-
-    // Refresh token and print the new time to expiration.
-    spotifyApi.refreshAccessToken().then(
-      function(data) {
-        tokenExpirationEpoch =
-          new Date().getTime() / 1000 + data.body['expires_in'];
-        console.log(
-          'Refreshed token. It now expires in ' +
-            Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
-            ' seconds!'
-        );
-      },
-      function(err) {
-        console.log('Could not refresh the token!', err.message);
-      }
-    );
-  }
-}, 1000);
-
-
+start_server()
 
 const Bot = new TwitchBot({
     username: 'crdbot',
@@ -88,14 +22,14 @@ async function get_current() {
 };
 
 
-async function song_request(name) {
+/* async function song_request(name) {
     spotifyApi.searchTracks(name)
     .then(function(data) {
       console.log(data.body);
     }, function(err) {
       console.error(err);
     });
-}
+} */
 
 
 
